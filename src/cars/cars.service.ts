db46d8fa0839321e0aeddacba35d63bf57398d79
josesantizo_dev <josesantizo149@gmail.com>
2025-04-20 22:16:54 -0600

    [ADD] exception not found added

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,34 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: 3,
+      brand: 'Nissan',
+      model: 'Altima',
+    },
+  ];
 
-    private cars = [
-        {
-            id: 1,
-            brand: 'Toyota',
-            model: 'Corolla',
-        },
-        {
-            id: 2,
-            brand: 'Honda',
-            model: 'Civic',
-        },
-        {
-            id: 3,
-            brand: 'Nissan',
-            model: 'Altima',
-        },
-    ]
+  findAll() {
+    return this.cars;
+  }
 
-    findAll() {
-        return this.cars;
-    }
+  findOne(id: number) {
+    const car = this.cars.find((car) => car.id === id);
 
-    findOne(id: number) {
-        return this.cars.find(car => car.id === id);
-    }
+    if (!car) throw new NotFoundException(`Car with ID ${id} not found`);
 
-
-
+    return car;
+  }
 }
